a3a7177865591d9a859b012b6961ab4a5dce05a4
hafidjs <afidzpratama@gmail.com>
2023-06-22 07:24:33 +0700

    first commit

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,66 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-calendar.html"
+                        aria-expanded="false"><i data-feather="calendar"></i><span
+                            class="hide-menu">Dashboard</span></a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-chats.html"
+                        aria-expanded="false"><i data-feather="message-circle"></i><span
+                            class="hide-menu">Pos Kasir</span></a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-contacts.html"
+                        aria-expanded="false"><i data-feather="phone"></i><span
+                            class="hide-menu">Live Chat</span></a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-invoice.html"
+                        aria-expanded="false"><i data-feather="file-text"></i><span
+                            class="hide-menu">Data</span></a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-notes.html"
+                        aria-expanded="false"><i data-feather="copy"></i><span
+                            class="hide-menu">Master</span></a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-taskboard.html"
+                        aria-expanded="false"><i data-feather="check-square"></i><span
+                            class="hide-menu">Kategori</span></a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-todo.html"
+                        aria-expanded="false"><i data-feather="trello"></i><span
+                            class="hide-menu">Produk</span></a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-taskboard.html"
+                        aria-expanded="false"><i data-feather="check-square"></i><span
+                            class="hide-menu">Laporan & Transaksi</span></a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-taskboard.html"
+                        aria-expanded="false"><i data-feather="check-square"></i><span
+                            class="hide-menu">Pengaturan</span></a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="app-taskboard.html"
+                        aria-expanded="false"><i data-feather="check-square"></i><span
+                            class="hide-menu">Clear All Cache</span></a>
+                </li>
+            </ul>
+
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+    <!-- Bottom points-->
+    <!-- End Bottom points-->
+</aside>
